b592b1aa7cf88d45192486f1fd4a8d023fa4b88e
Vane-byte <20180841@aloe.ulima.edu.pe>
2022-06-18 20:08:04 -0500

    Sincroniza data 1

---
+++ app/src/main/java/pe/edu/ulima/pm/ec/room/AppDatabase.kt
@@ -0,0 +1,27 @@
+package pe.edu.ulima.pm.swapp.room
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import pe.edu.ulima.pm.swapp.room.dao.PersonaRoomDAO
+import pe.edu.ulima.pm.swapp.room.models.PersonaRoom
+
+@Database(entities = arrayOf(PersonaRoom::class), version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun getPersonaRoomDAO() : PersonaRoomDAO
+
+    companion object {
+        private var mInstance : AppDatabase? = null
+
+        fun getInstance(context : Context) : AppDatabase {
+            if (mInstance == null) {
+                mInstance = Room.inMemoryDatabaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java
+                ).allowMainThreadQueries().build()
+            }
+            return mInstance!!
+        }
+    }
+}
