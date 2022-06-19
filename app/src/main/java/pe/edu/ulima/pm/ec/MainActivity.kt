8383695ceeb27f127a5fe925a00cacb7c541b8e9
sam <sam.perezf@hotmail.com>
2022-06-18 19:14:54 -0500

    avanzando ando

--- app/src/main/java/pe/edu/ulima/pm/ec/MainActivity.kt
+++ app/src/main/java/pe/edu/ulima/pm/ec/MainActivity.kt
@@ -2,10 +2,29 @@ package pe.edu.ulima.pm.ec
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import pe.edu.ulima.pm.ec.fragments.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val fragmentMain = MainFragment()
+    private val fragmentPersona = PersonaFragment()
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val ft = supportFragmentManager.beginTransaction()
+        ft.add(R.id.fcvFragments, fragmentMain)
+        ft.commit()
+
+        val bData = findViewById<Button>(R.id.butData)
+
+
+
     }
 }
---
+++ app/src/main/java/pe/edu/ulima/pm/ec/fragments/MainFragment.kt
@@ -0,0 +1,42 @@
+package pe.edu.ulima.pm.ec.fragments
+
+import android.os.Bundle
+import android.view.*
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import pe.edu.ulima.pm.ec.models.GestorPersona
+import kotlinx.coroutines.*
+import pe.edu.ulima.pm.ec.R
+
+class MainFragment: Fragment() {
+
+    private val fragmentPersona = PersonaFragment()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activity?.title = "Dashboard"
+        setHasOptionsMenu(true)
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val bData= view.findViewById<Button>(R.id.butData)
+
+        bData.setOnClickListener{
+            val fm= this.parentFragmentManager.beginTransaction()
+            fm.replace(R.id.fcvFragments, fragmentPersona)
+            fm.commit()
+
+        }
+    }
+
+}
