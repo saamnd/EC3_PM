7a3335d7331a33118dcb7b3ebf157f5b39432669
Vane-byte <20180841@aloe.ulima.edu.pe>
2022-06-19 01:27:11 -0500

    Sincroniza data 8

--- app/src/main/java/pe/edu/ulima/pm/ec/models/GestorPersona.kt
+++ app/src/main/java/pe/edu/ulima/pm/ec/models/GestorPersona.kt
@@ -1,28 +1,18 @@
 package pe.edu.ulima.pm.ec.models
 
-import android.app.Person
 import android.content.Context
 import android.os.Build
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.*
 import pe.edu.ulima.pm.ec.models.beans.Persona
 import android.os.Handler
 import android.util.Log
 import pe.edu.ulima.pm.ec.Constantes
-import android.view.View
-import android.widget.Button
 import android.widget.ProgressBar
 import androidx.annotation.RequiresApi
-import kotlinx.coroutines.*
-import pe.edu.ulima.pm.ec.R
-import pe.edu.ulima.pm.ec.fragments.MainFragment
 import pe.edu.ulima.pm.ec.room.*
 import pe.edu.ulima.pm.ec.room.dao.PersonaRoomDAO
-import pe.edu.ulima.pm.ec.room.models.DepartamentoRoom
 import pe.edu.ulima.pm.ec.room.models.PersonaRoom
 import java.net.HttpURLConnection
 import java.net.URL
-import java.text.SimpleDateFormat
 
 class GestorPersona {
 
@@ -36,7 +26,9 @@ class GestorPersona {
         val resultado = mutableListOf<String>()
         var cont=0;
         var data=""
-        while(br.readLine()!=null){
+
+        //while(br.readLine()!=null){
+        while(cont<500){
             data=br.readLine()
             resultado.add(data)
             cont++
@@ -59,14 +51,16 @@ class GestorPersona {
         persona.forEach {
             var result= it.split(";").map{it.trim()}
             cont++
+            var fecha= result[0].substring(6,8)+"-"+result[0].substring(4,6)+"-"+result[0].substring(0,4)
             daoPersona.insert(
                 PersonaRoom(
-                    result[0].substring(7)+"-"+result[0].substring(4,6)+"-"+result[0].substring(0,3),
+                    fecha,
                     result[1],
                     result[2],
                     result[3],
                     result[9],0)
             )
+            Log.d("result",fecha)
 
         }
 
@@ -78,6 +72,7 @@ class GestorPersona {
         Log.d("save", "Se guardó "+cont)
 
     }
+
     fun obtenerListaPersonasRoom (context : Context) : List<Persona> {
         val daoPersona : PersonaRoomDAO = AppDatabase.getInstance(
             context).getPersonaRoomDAO()
