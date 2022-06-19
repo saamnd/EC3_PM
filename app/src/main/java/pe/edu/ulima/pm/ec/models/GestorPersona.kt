d53af89f6e394e7788295787ecdbdaa00a7764b2
Vane-byte <20180841@aloe.ulima.edu.pe>
2022-06-18 21:03:19 -0500

    Sincroniza data 1

--- app/src/main/java/pe/edu/ulima/pm/ec/models/GestorPersona.kt
+++ app/src/main/java/pe/edu/ulima/pm/ec/models/GestorPersona.kt
@@ -5,32 +5,45 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.*
 import pe.edu.ulima.pm.ec.models.beans.Persona
 import android.os.Handler
+import android.util.Log
 import pe.edu.ulima.pm.ec.room.*
 import pe.edu.ulima.pm.ec.room.dao.PersonaRoomDAO
 import pe.edu.ulima.pm.ec.room.models.PersonaRoom
+import java.net.HttpURLConnection
+import java.net.URL
 
 class GestorPersona {
 
-    fun obtenerListaPersonasRoom(context : Context) : List<Persona> {
-        val daoPersona : PersonaRoomDAO = AppDatabase.getInstance(
-            context).getPersonaRoomDAO()
+    fun obtenerListaPersonasCorutina() : List<String> {
+        val url = URL("https://files.minsa.gob.pe/s/eRqxR35ZCxrzNgr/download")
+        val con = url.openConnection() as HttpURLConnection
 
-        val listaPersonasRoom = daoPersona.getAll() // consulta Room
-        println(listaPersonasRoom.size)
-        val listaPersonas = listaPersonasRoom.map {
-           Persona(it.fecha_corte, it.departamento, it.provincia, it.distrito, it.metododx)
+        var br=con.inputStream.bufferedReader()
+        val resultado = mutableListOf<String>()
+        var data=""
+        while(br.readLine()!=null){
+            data=br.readLine()
+            resultado.add(data)
+            Log.d("detail", data)
         }
-        return listaPersonas
+
+        return resultado
     }
 
-    fun guardarListaPersonasRoom(context : Context, persona : List<Persona>) {
+    fun guardarListaPersonasRoom(context : Context, persona : List<String>) {
         val db = AppDatabase.getInstance(
             context)
         val daoPersona : PersonaRoomDAO = db.getPersonaRoomDAO()
 
         persona.forEach {
             daoPersona.insert(
-                PersonaRoom(it.fecha_corte, it.departamento, it.provincia, it.distrito, it.metododx, 0)
+                PersonaRoom(
+                    persona[0],
+                    persona[1],
+                    persona[2],
+                    persona[3],
+                    persona[4],
+                    persona[5].toInt())
             )
         }
 
